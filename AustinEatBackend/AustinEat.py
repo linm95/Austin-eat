1f96bcba3ca2aa6a7851a30d681a5e1c94ca95c9
linm95 <linm95@utexas.edu>
2017-11-16 20:02:48 -0600

    lm

--- AustinEatBackend/AustinEat.py
+++ AustinEatBackend/AustinEat.py
@@ -9,5 +9,32 @@ import global_vars as VAR
 
 # [START LogIn]
 class Login(webapp2.RequestHandler):
-    
+    def get(self):
+        pass
 # [END LogIn]
+
+# [START DiscoverOrder]
+class DiscoverOrder(webapp2.RequestHandler):
+    def get(self):
+        pass
+    def post(self):
+        pass
+# [END DiscoverOrder]
+
+# [START OrderDetail]
+class OrderDetail(webapp2.RequestHandler):
+    def get(self):
+        pass
+# [End OrderDetail]
+
+class MyOrder(webapp2.RequestHandler):
+    def get(self):
+        pass
+    def post(self):
+        pass
+
+class MyProfile(webapp2.RequestHandler):
+    def get(self):
+        pass
+    def post(self):
+        pass
