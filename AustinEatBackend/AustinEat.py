6f084b68a36cf4b6496ae09ce73aa27b5b416a9a
Tony Tan <doublskyp@gmail.com>
2017-11-18 21:49:29 -0600

    first step of paypal

--- AustinEatBackend/AustinEat.py
+++ AustinEatBackend/AustinEat.py
@@ -45,6 +45,23 @@ class MyProfile(webapp2.RequestHandler):
         pass
 # [END MyProfile]
 
+# [START GetClientToken]
+class GetClientToken(webapp2.RequestHandler):
+    def get(self):
+        import braintree
+        import requests_toolbelt.adapters.appengine
+
+        # Use the App Engine Requests adapter. This makes sure that Requests uses
+        # URLFetch.
+        requests_toolbelt.adapters.appengine.monkeypatch()
+
+        gateway = braintree.BraintreeGateway(access_token=VAR.paypal_access_token)
+        client_token = gateway.client_token.generate()
+        self.response.write(client_token)
+# [END GetClientToken]
+
 # [START app]
-app = webapp2.WSGIApplication([], debug=True)
+app = webapp2.WSGIApplication([
+    (VAR.WALLET_URL, GetClientToken),
+], debug=True)
 # [END app]
--- AustinEatBackend/global_vars.py
+++ AustinEatBackend/global_vars.py
@@ -1,2 +1,5 @@
 # Paypal access token
 paypal_access_token = "access_token$sandbox$32qr9zb33rrs77yz$00c7cc6a12432f52cd124a22a9778b33"
+
+# Route
+WALLET_URL = "/wallet"
