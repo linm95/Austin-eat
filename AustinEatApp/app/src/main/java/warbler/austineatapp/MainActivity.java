450a69f03f9a942ed2ee5eedbd82516f1e0fc3ea
Tomoyokyou <lifeislikeamelody27@gmail.com>
2017-11-30 20:10:53 -0600

    Add empty order page

--- AustinEatApp/app/src/main/java/warbler/austineatapp/MainActivity.java
+++ AustinEatApp/app/src/main/java/warbler/austineatapp/MainActivity.java
@@ -12,20 +12,36 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     ProfileFragment profileFragment;
+    NoPropertyFragment noPropertyFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     return true;
                 case R.id.navigation_dashboard:
+
+                    if (UserHelper.getCurrentUserProperty().equals("deliver")){
+
+                    }
+                    else if(UserHelper.getCurrentUserProperty().equals("eater")){
+
+                    }
+                    else{
+                        noPropertyFragment = new NoPropertyFragment();
+                        fragmentTransaction.replace(R.id.content, noPropertyFragment);
+                        fragmentTransaction.commit();
+                    }
+
                     return true;
                 case R.id.navigation_notifications:
-                    FragmentManager fragmentManager = getFragmentManager();
-                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    //FragmentManager fragmentManager = getFragmentManager();
+                    //FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                     if (profileFragment == null) {
                         profileFragment = new ProfileFragment();
                     }
